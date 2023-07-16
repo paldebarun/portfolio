d1e07a52ef3045df444fab92a945ff6dcb2a7e78
Debarun Pal <debarunpal@Debaruns-MacBook-Air.local>
2023-07-16 12:35:08 +0530

    this is first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Full Stack Developer",
+      "Web Developer",
+      "UI-UX Designer",
+      "Backend Developer",
+      "Coder",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
+
+
+  
