f1890ed94c680c04f3c882fe484d6e02181fedea
Debarun Pal <debarunpal@Debaruns-MacBook-Air.local>
2023-08-07 23:48:22 +0530

    this is another commit

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var typeData = new Typed(".role", {
     backDelay: 1000,
   });
 
+  
+
 var burger_button=document.getElementById('burger-button');
 var cross_button=document.getElementById('cross-button');
 var sidebar=document.getElementsByClassName('nav-items')[0];
@@ -30,5 +32,51 @@ sidebar.classList.add("inactive");
 function openSidebar(){
   burger_button.classList.add("inactive");
   sidebar.classList.remove("inactive");
+}
+
+
+async function submitHandler(event){
+  console.log("inside this function");
+  event.preventDefault();
+  
+  const name = document.getElementById('nameInput').value;
+  const email = document.getElementById('emailInput').value;
+  const subject=document.getElementById('subjectInput').value;
+  const message=document.getElementById('messageInput').value;
+  console.log(name,email,subject,message);
+
+  const data = {
+    name,
+    email,
+    subject,
+    message
+  };
+
+  console.log(data);
+
+  
+
+  try {
+    const response = await fetch('http://localhost:4000/api/v1/submitfeedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    
+    console.log('Response from API:', response);
+
+
+  } catch (error) {
+    console.error('Error sending the API request:', error);
+   
+  }
+
 }
   
